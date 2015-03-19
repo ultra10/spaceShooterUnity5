6e2bd3c4377c0505f927b1f726a356b88d31d807
Marcin <marcin@pureholidayhomes.com>
2015-03-20 00:20:54 +0100

    new clean version

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	public int scoreValue;
+	private GameController gc;
+
+	void Start(){
+		GameObject gameControllerObj = GameObject.FindWithTag ("GameController");
+		if (gameControllerObj != null) {
+			gc = gameControllerObj.GetComponent<GameController>();
+		}
+		if (gc == null) {
+			Debug.Log("Nie działa :(");
+		}
+	}
+
+	/** 
+	 * @param other wzkazuje na obiekt który wywołuje ten skrypt 
+	 */
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Boundary") {
+			return;
+		}
+
+		Instantiate (explosion, transform.position, transform.rotation);
+		if (other.tag == "Player") {
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+			gc.GameOver();
+		}
+		gc.addScore (scoreValue);
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazard;
+	public Vector3 spownValues;
+	public int hazardCount;
+	public float spownWait;
+	public float startWait;
+	public float waveWait;
+
+	public GUIText scoreText;
+	public GUIText restartText;
+	public GUIText gameOverText;
+
+	private bool gameOver;
+	private bool restart;
+	private int score;
+
+	void Start()
+	{
+		gameOver = false;
+		restart = false;
+		restartText.text = "";
+		gameOverText.text = "";
+		score = 0;
+		updateScore ();
+		StartCoroutine(spawnWaves());
+	}
+
+	IEnumerator spawnWaves()
+	{
+		yield return new WaitForSeconds(startWait);
+		while(true)
+		{
+			for (int i = 0; i < hazardCount; i++) {
+				Vector3 spownPosition = new Vector3 (Random.Range (-spownValues.x, spownValues.x), spownValues.y, spownValues.z);
+				Quaternion spownRotation = Quaternion.identity;
+				Instantiate (hazard, spownPosition, spownRotation);
+
+				yield return new WaitForSeconds(spownWait);
+			}
+			yield return new WaitForSeconds(waveWait);
+
+			if(gameOver)
+			{
+				restartText.text = "Press 'R' to restart";
+				restart = true;
+				break;
+			}
+		}
+	}
+
+	void Update()
+	{
+		if (restart) {
+			if (Input.GetKeyDown(KeyCode.R))
+			{
+				Application.LoadLevel( Application.loadedLevel);
+			}
+		}
+	}
+
+	public void addScore(int newScoreValue)
+	{
+		score += newScoreValue;
+		updateScore ();
+	}
+
+	void updateScore()
+	{
+		scoreText.text = "Score: " + score;
+	}
+
+	public void GameOver()
+	{
+		gameOver = true;
+		gameOverText.text = "Game Over";
+	}
+}
